b8d7d33fb24e06a2eb47b18b0d42f79692612777
Keith Fernandes <72031382+keith-fernandes@users.noreply.github.com>
2023-12-04 13:53:16 +0000

    First Commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// Wrap your code inside an event listener for when the Typed library is loaded
+window.addEventListener('DOMContentLoaded', function() {
+    // Your JavaScript code using the Typed library
+
+var typed = new  Typed('.auto-type',{
+    strings: ["! To Keith's World!", "! You are Welcome here!", "! Fell home, but keep it organised!"],
+    typeSpeed:100,
+    backSpeed:100,
+    loop:true
+});
+});
+
+// function iconHyper(){
+//     var icon = document.getElementById('first');
+//     if()
+// }
